1b4c8d4f9e2a776aff97cf0a06f9ef5adcbc0340
Bar Segev <segevbar94@gmail.com>
2022-06-02 00:20:09 +0300

    debugging round 3

--- kmeans.c
+++ kmeans.c
@@ -21,12 +21,14 @@ PyObject *cToPy(Cluster *clusters, int k, int dim, int N)
 static PyObject *fit_capi(PyObject *self, PyObject *args);
 
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     return 0;
 }
 
 
-static PyObject *kmeans(int k, int max_iter, int dim_py, int N_py, PyObject *centroids_py, PyObject *vectors_py){
+static PyObject *kmeans(int k, int max_iter, int dim_py, int N_py, PyObject *centroids_py, PyObject *vectors_py)
+{
     int N = N_py;
     int dim = dim_py;
     Cluster* clusters;
@@ -38,25 +40,30 @@ static PyObject *kmeans(int k, int max_iter, int dim_py, int N_py, PyObject *cen
 
     /*convert k centroids from python to C*/
     clusters = (Cluster *)calloc(k, sizeof(Cluster));
-    if (clusters == NULL) {
+    if (clusters == NULL) 
+    {
         printf("An Error Has Occurred\n");
         exit(1);
     }
 
-    for (i = 0; i < k; i++){
+    for (i = 0; i < k; i++)
+    {
         clusters[i].centroid = (double *)calloc(dim, sizeof(double));
-        if (clusters[i].centroid == NULL) {
+        if (clusters[i].centroid == NULL)
+        {
             printf("An Error Has Occurred\n");
             exit(1);
         }
 
-        for (j = 0; j < dim; j++){
+        for (j = 0; j < dim; j++)
+        {
             clusters[i].centroid[j] = PyFloat_AsDouble(PyList_GetItem(centroids_py, cnt));
         }
 
         clusters[i].vectors_count = 0;
         clusters[i].vectors_sum = (double *)calloc(dim, sizeof(double));
-        if (clusters[i].vectors_sum == NULL){
+        if (clusters[i].vectors_sum == NULL)
+        {
             printf("An Error Has Occurred\n");
             exit(1);
         }
@@ -64,16 +71,20 @@ static PyObject *kmeans(int k, int max_iter, int dim_py, int N_py, PyObject *cen
 
     /*main loop*/
     cnt = 0;
-    while ((cnt < max_iter) && (!has_converged)) {
+    while ((cnt < max_iter) && (!has_converged))
+    {
 
         /*find current vector cluster*/
-        for (i = 0; i < N; i++) {
+        for (i = 0; i < N; i++)
+        {
             curr_vector = (double*)calloc(dim, sizeof(double));
-            if (curr_vector == NULL) {
+            if (curr_vector == NULL)
+            {
                 printf("An Error Has Occurred\n");
                 exit(1);
             }
-            for (j = 0; j < dim; j++){
+            for (j = 0; j < dim; j++)
+            {
                 curr_vector[j] = PyFloat_AsDouble(PyList_GetItem(vectors_py, cnt));
                 cnt++;
             }
@@ -85,9 +96,11 @@ static PyObject *kmeans(int k, int max_iter, int dim_py, int N_py, PyObject *cen
         has_converged = updateCentroids(clusters, k, dim);
         
         /*reset*/
-        for(i = 0; i < k; i++){
+        for(i = 0; i < k; i++)
+        {
             clusters[i].vectors_count = 0;
-            for(j = 0; j < dim; j++){
+            for(j = 0; j < dim; j++)
+            {
                 clusters[i].vectors_sum[j] = 0;
             }
         }
@@ -97,7 +110,8 @@ static PyObject *kmeans(int k, int max_iter, int dim_py, int N_py, PyObject *cen
     return cToPy(clusters, k, dim, N);
 }
 
-void calcCluster(double* vector, Cluster* clusters, int k, int dim) {
+void calcCluster(double* vector, Cluster* clusters, int k, int dim)
+{
     double min_distance = -1.0;
     int closest_cluster = -1;
     double distance;
@@ -105,9 +119,11 @@ void calcCluster(double* vector, Cluster* clusters, int k, int dim) {
     int j = 0;
 
     /*find closest cluster to current vector*/
-    for (i = 0; i < k; i++) {
+    for (i = 0; i < k; i++)
+    {
         distance = calcDistance(vector, clusters[i].centroid, dim);
-        if ((distance < min_distance) || (min_distance < 0)) {
+        if ((distance < min_distance) || (min_distance < 0))
+        {
             min_distance = distance; 
             closest_cluster = i;
         }
@@ -115,22 +131,26 @@ void calcCluster(double* vector, Cluster* clusters, int k, int dim) {
     
     /*update closest cluster*/
     clusters[closest_cluster].vectors_count++; 
-    for (j = 0; j < dim; j++) {
+    for (j = 0; j < dim; j++)
+    {
         clusters[closest_cluster].vectors_sum[j] += vector[j];
     }
 }
 
-double calcDistance(double* vector1, double* vector2, int dim) {
+double calcDistance(double* vector1, double* vector2, int dim)
+{
     double sum = 0.0;
     int j = 0;
 
-    for (j = 0; j < dim; j++) {
+    for (j = 0; j < dim; j++)
+    {
         sum += (vector1[j]-vector2[j])*(vector1[j]-vector2[j]);
     } 
     return sum;
 }
 
-int updateCentroids(Cluster* clusters, int k, int dim) {
+int updateCentroids(Cluster* clusters, int k, int dim)
+{
     double epsilon = 0.001;
     int i = 0;
     int j = 0;
@@ -139,19 +159,23 @@ int updateCentroids(Cluster* clusters, int k, int dim) {
     double dist = 0;
 
     /*calculate new centroid*/
-    for (i = 0; i < k; i++) {
+    for (i = 0; i < k; i++)
+    {
         new_centroid = (double*)calloc(dim, sizeof(double));
-        if (new_centroid == NULL) {
+        if (new_centroid == NULL)
+        {
             printf("An Error Has Occurred\n");
             exit(1);
         }
-        for (j = 0; j < dim; j++) {
+        for (j = 0; j < dim; j++)
+        {
             new_centroid[j] = (clusters[i].vectors_sum[j]/clusters[i].vectors_count);
         }
         dist = sqrt(calcDistance(clusters[i].centroid, new_centroid, dim));
 
         /*check if convergence did not accured*/
-        if (dist >= epsilon) {
+        if (dist >= epsilon)
+        {
             has_converged = 0;
         }
 
@@ -164,7 +188,8 @@ int updateCentroids(Cluster* clusters, int k, int dim) {
 }
 
 /*convert centroids from C to python*/
-PyObject *cToPy(Cluster *clusters, int k, int dim, int N){
+PyObject *cToPy(Cluster *clusters, int k, int dim, int N)
+{
     PyObject *clusters_py;
     int i = 0;
     int j = 0;
@@ -172,9 +197,11 @@ PyObject *cToPy(Cluster *clusters, int k, int dim, int N){
     PyObject *curr_vector;
 
     clusters_py = PyList_New(k);
-    for (i = 0; i < k; i++){
+    for (i = 0; i < k; i++)
+    {
         curr_vector = PyList_New(dim);
-        for (j = 0; j < dim; j++){
+        for (j = 0; j < dim; j++)
+        {
             value = Py_BuildValue("d", clusters[i].centroid[j]);
             PyList_SetItem(curr_vector, j, value);
         }
@@ -182,7 +209,8 @@ PyObject *cToPy(Cluster *clusters, int k, int dim, int N){
         PyList_SetItem(clusters_py, i, curr_vector);
     }
     /*free clusters memory*/
-    for (i = 0; i < k; i++){
+    for (i = 0; i < k; i++)
+    {
         free(clusters[i].centroid);
         free(clusters[i].vectors_sum);
     }
@@ -193,7 +221,8 @@ PyObject *cToPy(Cluster *clusters, int k, int dim, int N){
 
 
 /*fit() function. gets arguments from python to kmeans function*/
-static PyObject *fit_capi(PyObject *self, PyObject *args){
+static PyObject *fit_capi(PyObject *self, PyObject *args)
+{
     int k;
     int max_iter;
     int dim_py;
@@ -201,10 +230,12 @@ static PyObject *fit_capi(PyObject *self, PyObject *args){
     PyObject *centroids;
     PyObject *vectors_py;
 
-    if (!(PyArg_ParseTuple(args, "iiiiOO", &k, &max_iter, &dim_py, &N_py, &centroids, &vectors_py))){
+    if (!(PyArg_ParseTuple(args, "iiiiOO", &k, &max_iter, &dim_py, &N_py, &centroids, &vectors_py)))
+    {
         return NULL;
     }
-    if (!PyList_Check(centroids) || !PyList_Check(vectors_py)){
+    if (!PyList_Check(centroids) || !PyList_Check(vectors_py))
+    {
         return NULL;
     }
 
